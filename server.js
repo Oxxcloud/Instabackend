d23ecfb43f0234bc526596f86fcf54d43261ee6e
Oxxcloud <88257165+Oxxcloud@users.noreply.github.com>
2023-04-13 02:54:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = "7569" ;
+const PORT = "5000" ;
 const cors = require("cors");
 const mongoose = require("mongoose");
 const { MONGODB_URL } = require("./config");
